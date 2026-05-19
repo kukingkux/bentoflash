fcacf45cdaa41abc289a7b93bc6404528e116aeb
kukingkux <cbmamangbae@gmail.com>
2026-05-19 19:21:21 +0700

    first commit

---
+++ src/main/java/com/tup/bentoflash/BentoflashApplication.java
@@ -0,0 +1,13 @@
+package com.tup.bentoflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BentoflashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BentoflashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tup/bentoflash/BentoflashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tup.bentoflash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BentoflashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
