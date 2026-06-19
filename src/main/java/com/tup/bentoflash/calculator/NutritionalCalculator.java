fa9d75caa28f79bda4ea8b0a1d91cb615cfa7ac9
Hafidh Nuruddin Akbar <aiohto46@gmail.com>
2026-06-19 16:27:15 +0700

    code nutirisicalculator

---
+++ src/main/java/com/tup/bentoflash/calculator/NutritionalCalculator.java
@@ -0,0 +1,42 @@
+package com.tup.bentoflash.calculator;
+
+import com.tup.bentoflash.core.model.Ingredient;
+import com.tup.bentoflash.core.model.LocalCultureBento;
+import java.util.List;
+
+
+public class NutritionalCalculator {
+
+    public double calculateTotalCalories(LocalCultureBento bento) {
+        double totalCalories = 0.0;
+
+        List<Ingredient> ingredients = bento.getIngredients();
+
+        for (Ingredient ingredient : ingredients) {
+            totalCalories += ingredient.getCalories(); // calories bertipe int, auto-widening ke double
+        }
+
+        return totalCalories;
+    }
+
+    public String calculateMacros(LocalCultureBento bento) {
+        double totalProtein = 0.0;
+        double totalCarbs   = 0.0;
+        double totalFat     = 0.0;
+
+        List<Ingredient> ingredients = bento.getIngredients();
+
+        for (Ingredient ingredient : ingredients) {
+            totalProtein += ingredient.getProtein();
+            totalCarbs   += ingredient.getCarbs();
+            totalFat     += ingredient.getFat();
+        }
+
+        return String.format(
+            "Protein: %.2fg | Carbs: %.2fg | Fat: %.2fg",
+            totalProtein,
+            totalCarbs,
+            totalFat
+        );
+    }
+}
